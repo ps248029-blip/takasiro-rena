5a3f8e73cb18517647f913dc6481e17505f534ee
ps248029-blip <ps248029@g.sguc.ac.jp>
2026-01-24 12:25:13 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Python（Colab）側のAPIのURL（CloudflareのURLを毎回ここだけ更新）
-const API_BASE = "https://platform-oregon-byte-taste.trycloudflare.com";
+const API_BASE = "https://wheel-solely-civil-childrens.trycloudflare.com";
 
 // ページが読み込まれてから実行
 document.addEventListener("DOMContentLoaded", () => {
