2dde7ebfb92fa3aed5a825d6c8b4344c93cc43ac
ps248029-blip <ps248029@g.sguc.ac.jp>
2026-01-24 22:42:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Python（Colab）側のAPIのURL
-const API_BASE = "https://railway-bosnia-snap-respected.trycloudflare.com";
+const API_BASE = "https://optimization-wake-translator-bidding.trycloudflare.com";
 
 // ページが読み込まれてから実行
 document.addEventListener("DOMContentLoaded", () => {
