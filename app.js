e3e68f762ce7ef822f7ae73b076baa63ae79a2a9
ps248029-blip <ps248029@g.sguc.ac.jp>
2026-01-24 22:24:06 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Python（Colab）側のAPIのURL
-const API_BASE = "https://mic-cube-qualify-pack.trycloudflare.com";
+const API_BASE = "https://ruling-handles-added-rain.trycloudflare.com";
 
 // ページが読み込まれてから実行
 document.addEventListener("DOMContentLoaded", () => {
